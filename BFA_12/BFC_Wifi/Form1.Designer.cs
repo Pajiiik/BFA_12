a69fe4066f3f9169d27d92f6f7f93cf9fe072a4a
Pavel <86227916+Pajiiik@users.noreply.github.com>
2023-10-19 16:55:59 +0200

    Add files via upload

---
+++ BFA_12/BFC_Wifi/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace BFC_Wifi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Update = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(1024, 620);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 692);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(4, 5);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(147, 148);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(150, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1005, 154);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Red;
+            this.panel3.Location = new System.Drawing.Point(150, 154);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(2, 538);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Red;
+            this.panel4.Location = new System.Drawing.Point(152, 154);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1050, 2);
+            this.panel4.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.ForeColor = System.Drawing.Color.Red;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(153, 156);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(158, 120);
+            this.listBox1.TabIndex = 9;
+            this.listBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox1_DrawItem);
+            this.listBox1.MeasureItem += new System.Windows.Forms.MeasureItemEventHandler(this.listBox1_MeasureItem);
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Update
+            // 
+            this.Update.Interval = 10000;
+            this.Update.Tick += new System.EventHandler(this.Update_Tick);
+            // 
+            // button2
+            // 
+            this.button2.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(1024, 556);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 54);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "netsh";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1155, 692);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer Update;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ BFA_12/BFC_Wifi/Form1.cs
@@ -0,0 +1,373 @@
+﻿using System;       //C:\Users\stulhoferp.04\Source\Repos\BFA_12
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Collections.Specialized;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Media.Media3D;
+
+namespace BFC_Wifi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static char[] Match =            {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j' ,'k','l','m','n','o','p',
+                        'q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','C','L','M','N','O','P',
+                        'Q','R','S','T','U','V','X','Y','Z','!','?',' ','*','-','+'};
+        public static bool PingHost(string nameOrAddress)
+        {
+            bool pingable = false;
+            Ping pinger = null;
+            try
+            {
+                pinger = new Ping();
+                PingReply reply = pinger.Send(nameOrAddress);
+                pingable = reply.Status == IPStatus.Success;
+            }
+            catch (PingException)
+            {
+                return false;
+            }
+            finally
+            {
+                if (pinger != null)
+                {
+                    pinger.Dispose();
+                }
+            }
+            return pingable;
+        }
+
+
+        static async Task<string> ScanWifiNetworksAsync()
+        {
+            string output = "";
+            for (int i = 0; i < 10; i++)
+            { 
+                try
+                {    
+                    // Spustit nástroj netsh pro zjištění dostupných Wi-Fi sítí
+                    Process process = new Process();
+                    process.StartInfo.FileName = "netsh";
+                    process.StartInfo.Arguments = "wlan show networks mode=Bssid";
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.CreateNoWindow = true;
+                    process.Start();
+
+                    // Asynchronní čekání na ukončení procesu
+                    Task.Run(() => process.WaitForExit());
+                    // Přečíst výstup nástroje netsh
+                    output = output + process.StandardOutput.ReadToEnd();
+                    // Výstup obsahuje informace o dostupných Wi-Fi sítích
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message,"Error");
+                }
+            }
+            return output;
+        }
+
+        static HashSet<string> GetUniqueSsids(string input)
+        {
+            HashSet<string> uniqueSsids = new HashSet<string>();
+            string[] lines = input.Split('\n');
+
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("SSID"))
+                {
+                    // Použijte regulární výraz k extrakci názvu SSID
+                    Match match = Regex.Match(line, @"SSID (\d+) : (.+)");
+                    if (match.Success)
+                    {
+                        string ssid = match.Groups[2].Value.Trim();
+                        uniqueSsids.Add(ssid);
+                    }
+                }
+            }
+
+            return uniqueSsids;
+        }
+
+
+
+        string input;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("WI-FI:");
+            Task<string> wifiScan = ScanWifiNetworksAsync();
+            input = wifiScan.Result;
+            HashSet<string> uniqueSsids = GetUniqueSsids(input);
+
+            foreach (string ssid in uniqueSsids)
+            {
+                listBox1.Items.Add(ssid);
+                UpdateListBoxWidth();
+            }
+
+            if (PingHost("8.8.8.8"))
+            {
+
+            }
+            else
+            {
+
+            }
+            RemoveEmptyLinesFromListBox(listBox1);
+            Update.Start();
+        }
+        string selectedSsid = "";
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedSsid = listBox1.SelectedItem.ToString();
+        }
+        public static string GetWifiInfo(string ssid)
+        {
+            try
+            {
+                if (ssid != "WI-FI:")
+                {
+                    // Spustíme příkaz "netsh wlan show all" v konzoli Windows
+                    Process process = new Process
+                    {
+                        StartInfo = new ProcessStartInfo
+                        {
+                            FileName = "cmd.exe",
+                            RedirectStandardInput = true,
+                            RedirectStandardOutput = true,
+                            CreateNoWindow = true,
+                            UseShellExecute = false,
+                            WorkingDirectory = Environment.SystemDirectory
+                        }
+                    };
+                    process.Start();
+
+                    // Odešleme příkaz "netsh wlan show all" do konzole
+                    process.StandardInput.WriteLine("netsh wlan show all");
+
+                    // Počkáme na dokončení výstupu a získáme ho
+                    process.StandardInput.WriteLine("exit");
+                    string output = process.StandardOutput.ReadToEnd();
+                    process.WaitForExit();
+                    process.Close();
+                    string ssidToFind = ssid; // Zde zadejte hledané jméno Wi-Fi sítě
+                    string[] lines = output.Split('\n');
+
+                    bool found = false;
+                    List<string> wifiInfo = new List<string>();
+
+                    foreach (string line in lines)
+                    {
+                        if (line.Contains(ssidToFind))
+                        {
+                            found = true;
+                            wifiInfo.Add(line); // Přidejte řádek se začátkem informací o síti
+                        }
+                        else if (found)
+                        {
+                            if (string.IsNullOrWhiteSpace(line)) // Konec informací o síti
+                            {
+                                break;
+                            }
+                            wifiInfo.Add(line); // Přidejte další řádek informací
+                        }
+                    }
+
+                    if (found)
+                    {
+                        string wifiInfoText = string.Join("\n", wifiInfo);
+                        MessageBox.Show(wifiInfoText, "Informace o Wi-Fi síti");
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Wi-Fi síť s názvem \"{ssidToFind}\" nebyla nalezena.");
+                    }
+
+                    return output;
+                }
+                else
+                {
+                    RunAllNetshCommandsForWiFi();
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                return "Chyba při získávání informací o Wi-Fi: " + ex.Message;
+            }
+        }
+
+        static void RunAllNetshCommandsForWiFi()
+        {
+            // Vytvořte pole s příkazy netsh pro získání informací o Wi-Fi
+            string[] netshCommands = {
+            "wlan show interfaces",
+            "wlan show drivers",
+            "wlan show profiles",
+            "wlan show hostednetwork"
+        };
+
+            foreach (var command in netshCommands)
+            {
+                ExecuteNetshCommand(command);
+            }
+        }
+
+        static void ExecuteNetshCommand(string command)
+        {
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "netsh",
+                    Arguments = command,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            MessageBox.Show(output);
+            process.WaitForExit();
+        }
+
+        private void listBox1_MeasureItem(object sender, MeasureItemEventArgs e)
+        {
+            e.ItemHeight = 30;
+        }
+
+        private void listBox1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index >= 0)
+            {
+                e.DrawBackground();
+                e.Graphics.DrawString(listBox1.Items[e.Index].ToString(), e.Font, Brushes.Black, e.Bounds, StringFormat.GenericDefault);
+                e.DrawFocusRectangle();
+            }
+        }
+        private void UpdateListBoxWidth()
+        {
+            // Nastavte šířku ListBoxu na základě počtu položek
+            int maxWidth = 0;
+            foreach (var item in listBox1.Items)
+            {
+                int itemWidth = TextRenderer.MeasureText(item.ToString(), listBox1.Font).Width;
+                maxWidth = Math.Max(maxWidth, itemWidth);
+            }
+
+            listBox1.Width = maxWidth + SystemInformation.VerticalScrollBarWidth;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void RemoveEmptyLinesFromListBox(System.Windows.Forms.ListBox listBox)
+        {
+            for (int i = listBox.Items.Count - 1; i >= 0; i--)
+            {
+                if (string.IsNullOrWhiteSpace(listBox.Items[i].ToString()))
+                {
+                    listBox.Items.RemoveAt(i);
+                }
+            }
+        }
+
+        private void Update_Tick(object sender, EventArgs e)
+        {
+            Task<string> wifiScan = ScanWifiNetworksAsync();
+            string input = wifiScan.Result;
+            HashSet<string> uniqueSsids = GetUniqueSsids(input);
+            listBox1.Items.Clear();
+            listBox1.Items.Add("WI-FI:");
+            foreach (string ssid in uniqueSsids)
+            {
+                listBox1.Items.Add(ssid);
+                UpdateListBoxWidth();
+            }
+            RemoveEmptyLinesFromListBox(listBox1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (selectedSsid == "" || selectedSsid == "Wi-Fi")
+            {
+                RunAllNetshCommandsForWiFi();
+            }
+            else
+            {
+                GetWifiInfo(selectedSsid);
+            }
+        }
+        public static void ConnectToWifi(string ssid, string password)
+        {
+            // Připravíme příkazy pro připojení k Wi-Fi síti
+            string profileName = $"profileName=\"{ssid}\"";
+            string keyMaterial = $"keyMaterial=\"{password}\"";
+
+            // Vytvoříme profile pro Wi-Fi síť
+            RunNetshCommand($"wlan add profile {profileName} {keyMaterial}");
+
+            // Připojíme se k síti
+            RunNetshCommand($"wlan connect {ssid}");
+        }
+
+        private static void RunNetshCommand(string arguments)
+        {
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "BFA",
+                    Arguments = arguments,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+            process.WaitForExit();
+            process.Close();
+        }
+        static void Recurse(int Lenght, int Position, string BaseString)
+        {
+            int Count = 0;
+
+            int maxDegreeOfParallelism = Environment.ProcessorCount; // Nastavte počet vláken dle potřeby
+            List<string> results = new List<string>();
+
+            Parallel.ForEach(Match, new ParallelOptions { MaxDegreeOfParallelism = maxDegreeOfParallelism }, (item) =>
+            {
+                if (Position < Lenght - 1)
+                {
+                    string newBaseString = BaseString + item;
+                    Recurse(Lenght, Position + 1, newBaseString);
+
+                    ConnectToWifi(selectedSsid,newBaseString);
+                        results.Add(newBaseString);
+                    
+                }
+            });
+        }
+    }
+}
